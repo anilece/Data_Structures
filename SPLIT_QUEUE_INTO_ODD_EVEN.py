f1a8d3f5287d8c37068486d8068c1deca2520994
Anil_Kumar_R <47322405+anilece@users.noreply.github.com>
2019-06-15 11:47:42 +0530

    Create SPLIT_QUEUE_INTO_ODD_EVEN.py

---
+++ SPLIT_QUEUE_INTO_ODD_EVEN.py
@@ -0,0 +1,79 @@
+class Queue:
+    def __init__(self,max_size):
+        self.__max_size=max_size
+        self.__elements=[None]*self.__max_size
+        self.__rear=-1
+        self.__front=0
+    
+    def get_max_size(self):
+        return self.__max_size
+    
+    def is_full(self):
+        if(self.__rear==self.__max_size-1):
+            return True
+        return False
+    
+    def is_empty(self):
+        if(self.__front>self.__rear):
+            return True
+        return False
+    
+    def enqueue(self,data):
+        if(self.is_full()):
+            print("Queue is full!!!")
+        else:
+            self.__rear+=1
+            self.__elements[self.__rear]=data
+    
+    def dequeue(self):
+        if(self.is_empty()):
+            print("Queue is empty!!!")
+        else:
+            data=self.__elements[self.__front]
+            self.__front+=1
+            return data
+    
+    def display(self):
+        for index in range(self.__front, self.__rear+1):
+            print(self.__elements[index])
+                                       
+    def __str__(self):
+        msg=[]
+        index=self.__front
+        while(index<=self.__rear):
+            msg.append((str)(self.__elements[index]))
+            index+=1
+        msg=" ".join(msg)
+        msg="Queue data(Front to Rear): "+msg
+        return msg
+
+    
+def split_queue(num_queue):
+    queue_list=[]
+    N=num_queue.get_max_size()
+    queue_list1=Queue(N)
+    queue_list2=Queue(N)
+
+    for i in range(N):
+        temp=num_queue.dequeue()
+        if temp%2==1:
+            queue_list1.enqueue(temp)
+        else:
+            queue_list2.enqueue(temp)
+
+    queue_list.append(queue_list1)
+    queue_list.append(queue_list2)
+    return queue_list
+
+num_queue=Queue(7)
+num_queue.enqueue(2)
+num_queue.enqueue(7)
+num_queue.enqueue(9)
+num_queue.enqueue(4)
+num_queue.enqueue(6)
+num_queue.enqueue(5)
+num_queue.enqueue(10)
+
+q_list=split_queue(num_queue)
+q_list[0].display()
+q_list[1].display()
